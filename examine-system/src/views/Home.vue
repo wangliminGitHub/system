f5b86e90ae287022605ea7a3df4f38dd769e29f7
wanglimin <17629094718@163.com>
2019-09-25 21:03:24 +0800

    添加vant

--- examine-system/src/views/Home.vue
+++ examine-system/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
   </div>
 </template>
 
